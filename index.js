d95b68888cc991d15a03c49d408c3dafc1784b67
abicoder123 <ablor32@icstudents.org>
2026-02-27 15:39:55 -0600

    Refactor button event listeners for clarity

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-const button = document.getElementById("unicornbtn");
+// const button = document.getElementById("unicornbtn");
+// const message = document.getElementById("message");
+
+// button.addEventListener("click", function() {
+//   message.textContent = "YAY!!! You win!";
+// });
+
+// // aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// let points = 0;
+
+// const button = document.getElementById("scoreBtn");
+// const scoreText = document.getElementById("score");
+
+// button.addEventListener("click", function() {
+//   points = points + 1;
+//   scoreText.textContent = points;
+// });
+
+
+const unicornButton = document.getElementById("unicornbtn");
 const message = document.getElementById("message");
 
-button.addEventListener("click", function() {
+unicornButton.addEventListener("click", function() {
   message.textContent = "YAY!!! You win!";
 });
 
-// aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+
 let points = 0;
 
-const button = document.getElementById("scoreBtn");
+const scoreButton = document.getElementById("scoreBtn");
 const scoreText = document.getElementById("score");
 
-button.addEventListener("click", function() {
+scoreButton.addEventListener("click", function() {
   points = points + 1;
   scoreText.textContent = points;
 });
