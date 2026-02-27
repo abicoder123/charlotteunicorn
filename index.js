d466b02cfd067ef814899ae0b21d5ff6220d0700
abicoder123 <ablor32@icstudents.org>
2026-02-27 15:41:52 -0600

    Refactor event listener for unicorn button
    Removed commented-out code and cleaned up event listener assignments.

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-// const button = document.getElementById("unicornbtn");
-// const message = document.getElementById("message");
-
-// button.addEventListener("click", function() {
-//   message.textContent = "YAY!!! You win!";
-// });
-
-// // aaaaaaaaaaaaaaaaaaaaaaaaaaaa
-// let points = 0;
-
-// const button = document.getElementById("scoreBtn");
-// const scoreText = document.getElementById("score");
-
-// button.addEventListener("click", function() {
-//   points = points + 1;
-//   scoreText.textContent = points;
-// });
-
-
-const unicornButton = document.getElementById("unicornbtn");
+const button = document.getElementById("unicornbtn");
 const message = document.getElementById("message");
 
-unicornButton.addEventListener("click", function() {
+button.addEventListener("click", function() {
   message.textContent = "YAY!!! You win!";
 });
 
 
-
-let points = 0;
-
-const scoreButton = document.getElementById("scoreBtn");
-const scoreText = document.getElementById("score");
-
-scoreButton.addEventListener("click", function() {
-  points = points + 1;
-  scoreText.textContent = points;
-});
