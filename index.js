8a935026f315956309f975c3ce4d5b584e867878
abicoder123 <ablor32@icstudents.org>
2026-02-28 08:48:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const button = document.getElementById("unicornbtn");
 const message = document.getElementById("message");
-unicorn.addEventListener("click", function() {
-  message.textContent = "YAY!!! You win!";
-  gameOver = true;
-  clearInterval(moveInterval); // 🛑 stops movement
-});
+
 button.addEventListener("click", function() {
   message.textContent = "YAY!!! You win!";
 });
